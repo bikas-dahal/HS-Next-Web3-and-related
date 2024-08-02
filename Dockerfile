70dcdbc330d5040d2ba174ce993e2225cbffefe8
bikky <bikkydahal@gmail.com>
2024-08-02 16:07:07 +0545

    Basic change with docker compose

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,17 @@
-FROM node:20
-
+FROM node:20 AS base
+ENV MONGODB_URI=mongodb:27017
 WORKDIR /usr/src/app
-
-COPY . .
-
+COPY package*.json ./
 RUN npm install
 
-EXPOSE 3000
 
-CMD ["node", "index.js"]
+FROM base AS development
+COPY . .
+CMD ["npm", "run", "dev"]
+
+FROM base AS production
+COPY . .
+RUN npm prune --production
+CMD ["npm", "run", "start"]
+
+
--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
 const app = express()
 
 const port =3000
 
-app.get('/', (req, res) => {
-    res.sendFile('./index.html')
+mongoose.connect(process.env.MONGODB_URI, {});
+
+const EntrySchema = new mongoose.Schema({
+    text: String,
+    date: {
+        type: Date, default: Date.now
+    }
 })
+
+const Entry = mongoose.model('Entry', EntrySchema)
+
+
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
