8ca7be8c8b797599d943c97708a81944221189cb
bikky <bikkydahal@gmail.com>
2024-08-05 21:56:11 +0545

    Completed chapter 9 i.e. suspense and loading.tsx in nextjs learn

--- next/nextjs-dashboard/app/dashboard/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-    return <h1>hi dashboard here, hyalo</h1>
-}
--- next/nextjs-dashboard/app/lib/data.ts
+++ next/nextjs-dashboard/app/lib/data.ts
@@ -14,12 +14,12 @@ export async function fetchRevenue() {
     // Artificially delay a response for demo purposes.
     // Don't do this in production :)
 
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
+    console.log('Fetching revenue data...');
+    await new Promise((resolve) => setTimeout(resolve, 3000));
 
     const data = await sql<Revenue>`SELECT * FROM revenue`;
 
-    // console.log('Data fetch completed after 3 seconds.');
+    console.log('Data fetch completed after 3 seconds.');
 
     return data.rows;
   } catch (error) {
@@ -215,3 +215,4 @@ export async function fetchFilteredCustomers(query: string) {
     throw new Error('Failed to fetch customers table.');
   }
 }
+
