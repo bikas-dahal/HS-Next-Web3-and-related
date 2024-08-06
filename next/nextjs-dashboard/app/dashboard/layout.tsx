830c21a6c72b9c3760ca36e3d816c863414e39b3
bikky <bikkydahal@gmail.com>
2024-08-06 09:29:02 +0545

    Completed till chapter 11, i.e. Server and client component, static and dynamic rendering, streaming, ppr, search and pagination

--- next/nextjs-dashboard/app/dashboard/layout.tsx
+++ next/nextjs-dashboard/app/dashboard/layout.tsx
@@ -1,14 +1,16 @@
-import SideNav from "@/app/ui/dashboard/sidenav";
+import SideNav from "@/app/ui/dashboard/sidenav"; // Ensure this path is correct
+
+export const experimental_ppr = true;
 
 export default function Layout({children}: {children: React.ReactNode}) {
     return (
-        <div className='flex h-screen flex-col '>
-            <div className='h-full w-full flex-none md:w-64'>
+        <div className='flex h-screen flex-col md:flex-row'>
+            <div className='h-16 w-full flex-none bg-gray-50 md:h-full md:w-64'>
                 <SideNav />
             </div>
-            <div className='flex-grow p-6 md:overflow-y-auto md:p-12'>
+            <div className='flex-grow p-3 m-24 md:m-0 md:overflow-y-auto md:p-12 bg-white'>
                 {children}
             </div>
         </div>
-    )
+    );
 }
