698176b013bc7f095c39f6372ebbb7d92b0ca72f
bikky <bikkydahal@gmail.com>
2024-08-08 22:02:06 +0545

    MongoDB connected

--- next/essential/app/page.jsx
+++ next/essential/app/page.jsx
@@ -2,8 +2,13 @@ import Hero from "@/components/Hero";
 import InfoBoxes from "@/components/InfoBoxes";
 import Footer from "@/components/Footer";
 import HomeProperties from "@/components/HomeProperties";
+import connectDB from "@/config/database";
 
-const HomePage = () => {
+const HomePage = async () => {
+
+    await connectDB()
+    console.log(process.env.MONGODB_URI)
+
     return (
         <>
             <Hero />
