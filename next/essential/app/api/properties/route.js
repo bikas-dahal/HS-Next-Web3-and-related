2819d7fa2c3afe6b8da365d0c1d8634aaf75de55
bikky <bikkydahal@gmail.com>
2024-08-09 08:06:22 +0545

    Fixed basic typo for fetching data from db

---
+++ next/essential/app/api/properties/route.js
@@ -0,0 +1,26 @@
+import connectDB from "@/config/database";
+import Property from "@/models/Property"
+
+export const GET = async (request) => {
+    try {
+        await connectDB()
+
+        const properties = await Property.find({})
+
+        console.log(properties)
+
+        return new Response(JSON.stringify({properties}), {
+
+            status: 200
+        });
+    } catch (error) {
+        console.log(error)
+        return new Response (
+            'Something went wrong. Please try again later',
+            {
+                statusCode: 404,
+            }
+
+        )
+    }
+}
